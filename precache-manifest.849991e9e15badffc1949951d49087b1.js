90c38957d066f21fed37509b18fa4cecab48ce1f
yarishb <bogdan040105@gmail.com>
2020-07-19 13:37:10 +0300

    Updates

---
+++ precache-manifest.849991e9e15badffc1949951d49087b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e89971f43c94265ceb5318d1835099d",
+    "url": "/massageSambir/index.html"
+  },
+  {
+    "revision": "9321e8bb081b213ddae8",
+    "url": "/massageSambir/static/css/2.a8a1fc75.chunk.css"
+  },
+  {
+    "revision": "08ad4d53a6b1f5565100",
+    "url": "/massageSambir/static/css/main.04ca72ea.chunk.css"
+  },
+  {
+    "revision": "9321e8bb081b213ddae8",
+    "url": "/massageSambir/static/js/2.3c9f4e7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/massageSambir/static/js/2.3c9f4e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ad4d53a6b1f5565100",
+    "url": "/massageSambir/static/js/main.9497ef46.chunk.js"
+  },
+  {
+    "revision": "c04199eb5838aa478593",
+    "url": "/massageSambir/static/js/runtime-main.cf6aa341.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/massageSambir/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/massageSambir/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/massageSambir/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/massageSambir/static/media/slick.f97e3bbf.svg"
+  }
+]);
